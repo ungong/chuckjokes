3535924d8347983457175eceb4e4c4011c0126eb
DESKTOP-75SH258\rick <ricardo.castell.@gmx.com>
2021-10-19 20:25:46 +0200

    no message

---
+++ src/main/java/rcm/spfw/chuckjockes/ChuckjockesApplication.java
@@ -0,0 +1,13 @@
+package rcm.spfw.chuckjockes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckjockesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckjockesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rcm/spfw/chuckjockes/ChuckjockesApplicationTests.java
@@ -0,0 +1,13 @@
+package rcm.spfw.chuckjockes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckjockesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
